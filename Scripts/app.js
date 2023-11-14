ca37ce39c3f0bf1048846cb1f1ba923b9e9d631b
James  Smith <james_ad_smith@outlook.com>
2023-11-14 03:00:40 +0000

    Added responsive navbar with login and menu

---
+++ Scripts/app.js
@@ -0,0 +1,28 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    const profileBtn = document.getElementById("user-menu-button");
+    const profileMenu = document.getElementById("profile_menu");
+    const mobileMenuBtn = document.getElementById("mobile-menu-btn");
+    const mobileMenu = document.getElementById("mobile-menu");
+    const mobileMenuIcons = document.getElementsByClassName("menu-icon");
+    var profileMenuIsActive = false;
+
+    profileBtn.addEventListener("click", () => {
+        if (profileMenuIsActive == false) {
+            profileMenu.classList.remove("hidden");
+            profileBtn.classList.add("border-2", "border-solid", "border-white");
+            profileMenuIsActive = !profileMenuIsActive;
+        } else {
+            profileMenu.classList.add("hidden");
+            profileBtn.classList.remove("border-2", "border-solid", "border-white");
+            profileMenuIsActive = !profileMenuIsActive;
+        }
+    });
+
+    mobileMenuBtn.addEventListener("click", () => {
+        Array.from(mobileMenuIcons).forEach((icon) => {
+            icon.classList.toggle("hidden");
+            icon.classList.toggle("block");
+        });
+        mobileMenu.classList.toggle("hidden");
+    })
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,36 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./*.{html,js,php}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
 
+module.exports = {
+  content: [
+    "./*.{html,js,php}",
+    "./index.html",
+  ],
+   theme: {
+    extend: {
+      colors: {
+        'primary': {
+          100: '#327FAE',
+          200: '#2e739e',
+          300: '#29688E',
+          400: '#255C7E',
+          500: '#1e4b67',
+        },
+        'secondary': {
+          100: '#FEC29A',
+          200: '#FEB685',
+          300: '#FEA971',
+          400: '#FE9D5D',
+          500: '#fe9148',
+        },
+        'tertiary': {
+          100: '#FEE9AE',
+          200: '#FEE39A',
+          300: '#FEDE85',
+          400: '#FED872',
+          500: '#fdd35d',
+        },
+      },
+    },
+},
+   plugins: [],
+}
